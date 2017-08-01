8b49a3059f0e8f930a785552679290c6365ade3c
chrisr3d <chris.studer.68@gmail.com>
2017-08-01 13:45:20 +0200

    Added some commands usefull for potiron installation and configuration

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,7 +14,7 @@ apt-get install -y python3-pip libgeoip-dev > /dev/null 2>&1
 
 apt-get install -y git npm nodejs-legacy redis-server parallel > /dev/null 2>&1
 echo -e "\n--- Install tshark ---\n"
-DEBIAN_FRONTEND=noninteractive apt-get -y install tshark
+DEBIAN_FRONTEND=noninteractive apt-get -y install tshark > /dev/null 2>&1
 
 echo -e "\n--- Retrieving potiron ---\n"
 git clone https://github.com/CIRCL/potiron.git > /dev/null 2>&1
@@ -26,8 +26,19 @@ chmod -R 700 potiron
 cd potiron/
 
 echo -e "\n--- Installing potiron ---\n"
-pip3 install -r requirements.txt
-npm install -g phantomjs-prebuilt
+pip3 install -r requirements.txt > /dev/null 2>&1
+npm install -g phantomjs-prebuilt > /dev/null 2>&1
+redis-cli shutdown
+chown ubuntu /etc/redis/redis.conf
+chgrp ubuntu /etc/redis/redis.conf
+chmod 700 /etc/redis/redis.conf
+chown ubuntu /var/log/redis/redis-server.log
+chgrp ubuntu /var/log/redis/redis-server.log
+chmod 700 /var/log/redis/redis-server.log
+cat /etc/redis/redis.conf | sed -e "s_# unixsocket /var/run/redis/redis.sock_unixsocket /tmp/redis.sock_g" | sed -e "s_# unixsocketperm_unixsocketperm_g" > redis.conf
+cat redis.conf > /etc/redis/redis.conf
+redis-server /etc/redis/redis.conf
+update-rc.d redis-server enable
 # cd ./var/www
 # bash ./update_thirdparty.sh
 # some errors in this script : http 404 not found...
